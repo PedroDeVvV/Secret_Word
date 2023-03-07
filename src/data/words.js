bfa0182dc0a20d8d86bde430a1b94ad4a6d303e9
PedroDeVvV <pedrohgs2004@gmail.com>
2023-03-07 13:45:14 -0300

    New data

--- src/data/words.js
+++ src/data/words.js
@@ -1,10 +1,24 @@
 export const wordsList = {
-    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
-    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
-    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
-    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
-    programação: ["Linguagem", "Framework", "JavaScript", "React"],
-    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
-    marcas_de_carro: ["Audi", "Kia", "Pagani", "Ford", "Koegnisseg", "Bugatti"],
-    Exclusivos_Sony: ["God-Of-War", "Days-Gone", "Astro-Bot"]
-  };
+  partes_de_carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+  marcas_de_carro: ["Audi", "Kia", "Pagani", "Ford", "Koegnisseg", "Bugatti"],
+  Países: [
+    "Australia",
+    "Espanha",
+    "Turquia",
+    "Bulgaria",
+    "Nigeria",
+    "Azerbaijão",
+    "Inglaterra",
+    "Holanda",
+    "Uruguai",
+    "Argentina",
+    "Chile",
+    "Equador",
+    "Catar",
+  ],
+};
